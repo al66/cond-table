98bedca702ae78eead3935ec67e12e08caba3f30
al66 <andreas.leinen@team-ag.de>
2017-05-21 17:28:06 +0200

    Added addtional tests

--- test/test.js
+++ test/test.js
@@ -3,52 +3,49 @@
 var expect = require('chai').expect;
 var Table = require('../index').Table;
 
-var testTable = {
-    description: 'Simple table',
-    hitPolicy: 'Unique',
-    input: {
-        'x': {
-            description: 'Input x',
-            type: 'number'
-        },
-        'y': {
-            description: 'Input y',
-            type: 'number',
-            default: 2
-        }
-    },
-    output: {
-        'z': {
-            description: 'Output z',
-            type: 'number'
-        }
-    },
-    rules: {
-        'First rule': {
-            'x': '< 5, [20..30]',
-            'y': '> 6,[30..40]',
-            'z': '7'
-        },
-        'Second rule': {
-            'x': '> 5',
-            'z': '2'
-        }
-    }
-};
 
-var testData = [
-    { 'x': '3', 'y':'2', 'z':null},
-    { 'x': '3', 'y':'7', 'z':'7'},
-    { 'x': '6', 'y':'7', 'z':'2'},
-    { 'x': '20', 'y':'37', 'z':'7'},
-    { 'x': '6', 'z':'2'}
-]
 
 describe('Compile table', function() {
+    let testTable = {
+        description: 'Simple table',
+        hitPolicy: 'Unique',
+        input: {
+            'x': {
+                description: 'Input x',
+                type: 'number'
+            },
+            'y': {
+                description: 'Input y',
+                type: 'number',
+                default: 2
+            }
+        },
+        output: {
+            'z': {
+                description: 'Output z',
+                type: 'number'
+            }
+        },
+        rules: {
+            'First rule': {
+                'x': '< 5, [20..30]',
+                'y': '> 6,[30..40]',
+                'z': '7'
+            },
+            'Second rule': {
+                'x': '> 5',
+                'z': '2'
+            }
+        }
+    };
     describe('Init class', function() {
         it('Init without table', function(done){
             let table = new Table();
-            //expect(table).to.be.equals(expected);
+            done();
+        });
+        it('Init table', function(done){
+            let table = new Table(testTable);
+            expect(table.table.rules).to.be.equals(testTable.rules);
             done();
         });
         it('Call compile', function(done){
@@ -62,6 +59,46 @@ describe('Compile table', function() {
 });
 
 describe('Test table 1', function() {
+    let testTable = {
+        description: 'Simple table',
+        hitPolicy: 'Unique',
+        input: {
+            'x': {
+                description: 'Input x',
+                type: 'number'
+            },
+            'y': {
+                description: 'Input y',
+                type: 'number',
+                default: 2
+            }
+        },
+        output: {
+            'z': {
+                description: 'Output z',
+                type: 'number'
+            }
+        },
+        rules: {
+            'First rule': {
+                'x': '< 5, [20..30]',
+                'y': '> 6,[30..40]',
+                'z': '7'
+            },
+            'Second rule': {
+                'x': '> 5',
+                'z': '2'
+            }
+        }
+    };
+    let testData = [
+        { 'x': '3', 'y':'2', 'z':null},
+        { 'x': '3', 'y':'7', 'z':'7'},
+        { 'x': '6', 'y':'7', 'z':'2'},
+        { 'x': '20', 'y':'37', 'z':'7'},
+        { 'x': '6', 'z':'2'}
+    ];
+
     let table = new Table();
     table.compile(testTable);
     for (let i=0; i< testData.length; i++) {
@@ -73,62 +110,62 @@ describe('Test table 1', function() {
     }
 });
 
-testTable = {
-    description: 'Applicant Risk rating',
-    hitPolicy: 'Unique',
-    input: {
-        'age': {
-            description: 'Applicant Age',
-            type: 'number'
-        },
-        'hist': {
-            description: 'Medical History',
-            type: 'string'
-        }
-    },
-    output: {
-        'risk': {
-            description: 'Risk Rating',
-            type: 'string'
-        }
-    },
-    rules: {
-        'older 60 / good': {
-            'age': '>60',
-            'hist': 'good',
-            'risk': 'Medium'
-        },
-        'older 60 / bad': {
-            'age': '>60',
-            'hist': 'bad',
-            'risk': 'High'
-        },
-        'mid age': {
-            'age': '[25..60]',
-            'risk': 'Medium'
-        },
-        'kids / good': {
-            'age': '<25',
-            'hist': 'good',
-            'risk': 'Low'
-        },
-        'kids / bad)': {
-            'age': '<25',
-            'hist': 'bad',
-            'risk': 'Medium'
-        }
-    }
-};
-
-testData = [
-    { 'age': '65', 'hist':'good', 'risk':'Medium'},
-    { 'age': '70', 'hist':'bad', 'risk':'High'},
-    { 'age': '26', 'hist':'good', 'risk':'Medium'},
-    { 'age': '20', 'hist':'bad', 'risk':'Medium'},
-    { 'age': '10', 'hist':'good', 'risk':'Low'}
-];
 
 describe('Test table 2', function() {
+    let testTable = {
+        description: 'Applicant Risk rating',
+        hitPolicy: 'Unique',
+        input: {
+            'age': {
+                description: 'Applicant Age',
+                type: 'number'
+            },
+            'hist': {
+                description: 'Medical History',
+                type: 'string'
+            }
+        },
+        output: {
+            'risk': {
+                description: 'Risk Rating',
+                type: 'string'
+            }
+        },
+        rules: {
+            'older 60 / good': {
+                'age': '>60',
+                'hist': 'good',
+                'risk': 'Medium'
+            },
+            'older 60 / bad': {
+                'age': '>60',
+                'hist': 'bad',
+                'risk': 'High'
+            },
+            'mid age': {
+                'age': '[25..60]',
+                'risk': 'Medium'
+            },
+            'kids / good': {
+                'age': '<25',
+                'hist': 'good',
+                'risk': 'Low'
+            },
+            'kids / bad)': {
+                'age': '<25',
+                'hist': 'bad',
+                'risk': 'Medium'
+            }
+        }
+    };
+    let testData = [
+        { 'age': '65', 'hist':'good', 'risk':'Medium'},
+        { 'age': '70', 'hist':'bad', 'risk':'High'},
+        { 'age': '26', 'hist':'good', 'risk':'Medium'},
+        { 'age': '20', 'hist':'bad', 'risk':'Medium'},
+        { 'age': '10', 'hist':'good', 'risk':'Low'}
+    ];
+
     let table = new Table();
     table.compile(testTable);
     for (let i=0; i< testData.length; i++) {
@@ -138,4 +175,97 @@ describe('Test table 2', function() {
             done();
         });
     }
+});
+
+
+describe('Incomplete Table', function() {
+    let testTable = {
+        description: 'Simple table',
+        hitPolicy: 'Unique',
+        input: {
+            'x': {
+                description: 'Input x',
+                type: 'number'
+            },
+            'y': {
+                description: 'Input y',
+                type: 'number',
+                default: 2
+            }
+        },
+        output: {
+            'z': {
+                description: 'Output z',
+                type: 'number'
+            }
+        },
+        rules: {
+            'First rule': {
+                'x': '< 5, [20..30]',
+                'y': '> 6,[30..40]',
+                'z': '7'
+            },
+            'Second rule': {
+                'x': '> 5',
+                'z': '2'
+            }
+        }
+    };
+    it('Missing input', function(done) {
+        let testData = [
+            { 'x': '3', 'y':'2', 'z':null},
+            { 'x': '3', 'y':'7', 'z':null},
+            { 'x': '6', 'y':'7', 'z':null},
+            { 'x': '20', 'y':'37', 'z':null},
+            { 'x': '6', 'z':null}
+        ];
+        let t = testTable;
+        let d = testData;
+        t.input = null;
+        let table = new Table();
+        table.compile(t);
+        for (let i=0; i< d.length; i++) {
+            let result = table.result(d[i]);
+            expect(result.z).to.be.equals(d[i].z);
+        }
+        done();
+    });
+    it('Missing output', function(done) {
+        let testData = [
+            { 'x': '3', 'y':'2'},
+            { 'x': '3', 'y':'7'},
+            { 'x': '6', 'y':'7'},
+            { 'x': '20', 'y':'37'},
+            { 'x': '6'}
+        ];
+        let t = testTable;
+        let d = testData;
+        t.output = null;
+        let table = new Table();
+        table.compile(t);
+        for (let i=0; i< d.length; i++) {
+            let result = table.result(d[i]);
+            expect(result.z).to.be.undefined;
+        }
+        done();
+    });
+    it('Missing rules', function(done) {
+        let testData = [
+            { 'x': '3', 'y':'2'},
+            { 'x': '3', 'y':'7'},
+            { 'x': '6', 'y':'7'},
+            { 'x': '20', 'y':'37'},
+            { 'x': '6'}
+        ];
+        let t = testTable;
+        let d = testData;
+        t.rules = null;
+        let table = new Table();
+        table.compile(t);
+        for (let i=0; i< d.length; i++) {
+            let result = table.result(d[i]);
+            expect(result.z).to.be.undefined;
+        }
+        done();
+    });
 });
